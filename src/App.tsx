41a01e26f20512c57d20b1022ed46d4eed03d523
D_Zucho <danny.nico.dev@outlook.com>
2023-12-08 13:33:05 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>hi</p>
+    </>
+  )
+}
+
+export default App
